ff5cac920045cedb343b07b0fb18ce95a58c8921
Juli <julianleal306@gmail.com>
2026-06-20 20:32:08 -0300

    refactor: cambie el config y el entorno

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-SECRET_KEY = "POO-2026"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///datos.sqlite3'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///default.sqlite3")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
